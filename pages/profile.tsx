73b6bdd5b8b61fa4707d1b6dc914ca95a5571197
Luca Anequini Antoniazzi <lucanequini@gmail.com>
2024-04-16 01:26:23 -0300

    create user info page with responsive design (forms to edit info), the connection with the backend still missing

---
+++ pages/profile.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import UserForm from "@/src/components/profile/user";
+import HeaderAuth from "@/src/components/common/headerAuth";
+import { Container, Row, Col, Button } from "reactstrap";
+import styles from '../styles/profile.module.scss'
+import Footer from "@/src/components/common/footer";
+
+const Profile = () => {
+    return (
+        <>
+            <Head>
+                <title>OneBitFlix - Meus Dados</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            </Head>
+            <main>
+                <div className={styles.header}>
+                    <HeaderAuth />
+                </div>
+                <Container className="py-5">
+                    <p className={styles.title}>Minha Conta</p>
+                    <Row className="pt-3 pb-5">
+                        <Col md={4} className={styles.btnColumn}>
+                            <Button className={styles.renderForm}>DADOS PESSOAIS</Button>
+                            <Button className={styles.renderForm}>SENHA</Button>
+                        </Col>
+                        <Col md><UserForm /></Col>
+                    </Row>
+                </Container>
+                <div className={styles.footer}>
+                    <Footer />
+                </div>
+            </main>
+        </>
+    )
+
+}
+
+export default Profile
